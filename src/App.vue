c300fea861e8e3024968c152002f946deea99ff9
RayhanAmin26 <rayhanamin.lm2001@gmail.com>
2025-05-15 11:42:55 +0600

    app

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+const country = "Russia"
+const capital = "Mwosko"
+
 </script>
+
 
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="RAYHAN did it!" />
+      <h1>Welcome to {{ country }}</h1>
+      <p>The capital is {{ capital }} </p>
+     
     </div>
   </header>
 
@@ -18,30 +22,5 @@ import TheWelcome from './components/TheWelcome.vue'
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
