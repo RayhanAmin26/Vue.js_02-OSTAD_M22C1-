cda65b0714275197b1cf7503285a6cd42dc90fc5
RayhanAmin26 <rayhanamin.lm2001@gmail.com>
2025-05-15 12:23:52 +0600

    app2

---
+++ src/components/App2.vue
@@ -0,0 +1,27 @@
+<script setup>
+const country = "Russia"
+const capital = "Mwosko"
+
+</script>
+
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <!-- <h1>Welcome to {{ country }}</h1>
+      <p>The capital is {{ capital }} </p>  -->
+      <AlertComponent/>
+     
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+
+</style>
